c8adfa681fa191b5465370de5cd14d9d1077c325
idojustdoit <yulchan9478@gmail.com>
2023-12-21 23:29:11 +0900

    [feat] Todo list 생성/삭제기능 구현

--- vue_playground/src/components/HelloWorld.vue
+++ vue_playground/src/components/HelloWorld.vue
@@ -1,38 +1,58 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    <h1>Todo List</h1>
+    <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new todo" />
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo.text }}
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
+<script >
+import { ref, onMounted } from 'vue';
+
+export default {
+  setup() {
+    const newTodo = ref("");
+    const todos = ref([]);
+
+    onMounted(() => {
+      // 페이지 로드 시에 Local Storage에서 데이터 불러오기
+      const savedTodos = localStorage.getItem("todos");
+      if (savedTodos) {
+        todos.value = JSON.parse(savedTodos);
+      }
+    });
+
+    const addTodo = () => {
+      if (newTodo.value.trim() === "") return;
+      todos.value.push({ text: newTodo.value });
+      newTodo.value = "";
+      saveTodos();
+    };
+
+    const removeTodo = (index) => {
+      todos.value.splice(index, 1);
+      saveTodos();
+    };
+
+    const saveTodos = () => {
+      // Local Storage에 데이터 저장
+      localStorage.setItem("todos", JSON.stringify(todos.value));
+    };
+
+    return {
+      newTodo,
+      todos,
+      addTodo,
+      removeTodo,
+    };
+  },
+};
+</script>
 <style scoped>
 .read-the-docs {
   color: #888;
