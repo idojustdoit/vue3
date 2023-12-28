8b942fdaebc88400ef9a98e5c3acf2e0be85ef37
idojustdoitrsync <yulchan9478@gmail.com>
2023-12-28 17:30:49 +0900

    [docs] 회원가입 파이어베이스 핸드폰인증

--- vue_playground/src/components/Login.vue
+++ vue_playground/src/components/Login.vue
@@ -2,20 +2,21 @@
     <div class="centerTab">
     로그인 페이지
       <div>
-        <span>아이디</span>
-        <input v-model="id" type="email" />
-      </div>
-      <div>
-        <span>비밀번호</span>
-        <input v-model="pwd" type="text"/>
+        <span>핸드폰번호</span>
+        <input v-model="InputPhoneNumber" type="email" />
+        <div @click="login">로그인</div>
       </div>
     </div>
   </template>
   <script setup>
   import{ ref } from'vue';
   
-  const id = ref('');
-  const pwd = ref(null);
+  const InputPhoneNumber = ref('');
+
+
+  const login =()=> {
+    
+  }
   </script>
   
   <style>
