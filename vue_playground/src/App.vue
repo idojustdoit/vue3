b7540b166d5539712dddd2788ce48831e7281496
idojustdoitrsync <yulchan9478@gmail.com>
2024-01-02 18:14:36 +0900

    [feat] 전역관리상태 피니아추가 , 회원가입기능추가

--- vue_playground/src/App.vue
+++ vue_playground/src/App.vue
@@ -1,4 +1,17 @@
 <script setup>
+import{onMounted }from "vue";
+import { getAuth } from "firebase/auth";
+const auth = getAuth();
+const user = auth.currentUser;
+
+onMounted(()=>{
+  if (user !== null) {
+    console.log('로그인한 유저입니다.',user);
+  }else{
+    console.log('로그인 하지않은 유저입니다.');
+  }
+})
+
 </script>
 
 <template>
--- vue_playground/src/components/Signup.vue
+++ vue_playground/src/components/Signup.vue
@@ -5,6 +5,7 @@
       <div> 
 
       </div>
+  <input class="login_input" type="text" v-model="nickName" placeholder="닉네임을 입력해주세요"/>
   <input class="login_input" type="email" v-model="email" placeholder="이메일을 입력해주세요."/>
   <input class="login_input" type="text" v-model="pwd" placeholder="비밀번호를 입력해주세요."/>
   <div @click="signUp">회원가입 하기</div>
@@ -12,29 +13,36 @@
 </template>
 <script setup>
 import{ ref } from'vue';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword ,updateProfile  } from "firebase/auth";
 import { useRouter } from 'vue-router';
 
 
 const router = useRouter();
 const email = ref('');
 const pwd = ref ('');
-
-
+const nickName = ref('')
 const auth = getAuth();
+
+
 
 const signUp = () => { 
   createUserWithEmailAndPassword(auth, email.value, pwd.value)
   .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user);
-    router.push({path:'/login'})
+  console.log(userCredential);
+    updateProfile(auth.currentUser, {
+  displayName: nickName.value, 
+}).then(() => {
+  alert('회원가입이 완료되었습니다!')
+  router.push({path:'/login'});
+}).catch((error) => {
+    console.log('error_msg'+ error);
+});
+   
   })
   .catch((error) => {
     console.log(error.code);
-    if(error.message == 'Firebase: Error (auth/invalid-email).')
-{alert('이메일과 비밀번호를 확인해주세요')
+    if(error.message == 'Firebase: Error (auth/invalid-email).'){
+      alert('이메일과 비밀번호를 확인해주세요')
   }else{
     alert(error.message);
   }
